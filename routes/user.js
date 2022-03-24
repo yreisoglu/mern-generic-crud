2b80e945c8fdb84b8d3cbc07dfeffd16438c6dfb
Yasin Reisoğlu <100337130+yreisoglu@users.noreply.github.com>
2022-03-24 15:36:01 +0300

    encrypted routes updated

--- routes/user.js
+++ routes/user.js
@@ -1,33 +1,80 @@
 const express = require("express");
 const UserModel = require("../models/UserModel");
 const router = express.Router();
+const Cryptr = require('cryptr');
 
 
 router.get("/", (req, res) => {
-    res.send("user router test");
-})
-
-
-router.get("/get-users", (req, res) => {
+    const decryptedResponse = []
     UserModel.find().sort({ $natural: -1 }).limit(10)
-        .then(response => res.json(response))
+        .then(response => {
+            response.forEach((item) => {
+                decryptedResponse.push(decryptResponse(item,req.headers.userssecretkey))
+            })
+            res.json(decryptedResponse)
+        })
         .catch(error => console.log(error));
 })
 
 router.get("/get-user-by-id", (req, res) => {
+    const usersSecretKey = req.headers.userssecretkey;
     UserModel.findById(req.query.id)
-        .then(response => res.json(response))
+        .then(response => {
+            try {
+                res.json(decryptResponse(response, usersSecretKey))
+            }
+            catch {
+                res.json({ "error": "Bad Authenticate data" })
+                res.statusCode = "401"
+            }
+
+        })
         .catch(error => console.log(error))
 })
 
-router.post("/save-user", (req, res) => {
-    const user = new UserModel(req.body)
+router.post("/", (req, res) => {
+    const usersSecretKey = req.headers.userssecretkey;
+    const user = new UserModel(encryptBody(req.body, usersSecretKey))
     user.save()
         .then(response => res.json(response))
         .catch(error => {
             console.log(error);
-            res.status(404)
+            res.statusCode = "404"
         })
 })
+
+const decryptResponse = (response, usersSecretKey) => {
+    const cryptr = new Cryptr(usersSecretKey);
+    const decryptedResponse = {
+        name: cryptr.decrypt(response.name),
+        surname: cryptr.decrypt(response.surname),
+        email: cryptr.decrypt(response.email),
+        image: cryptr.decrypt(response.image),
+        totalWorkTime: cryptr.decrypt(response.totalWorkTime),
+        university: cryptr.decrypt(response.university),
+        previousJob: cryptr.decrypt(response.previousJob),
+        skills: cryptr.decrypt(response.skills),
+        description: cryptr.decrypt(response.description),
+    }
+    return decryptedResponse
+}
+const encryptBody = (body, usersSecretKey) => {
+    const cryptr = new Cryptr(usersSecretKey);
+    const encryptedBody = {
+        name: cryptr.encrypt(body.name),
+        surname: cryptr.encrypt(body.surname),
+        email: cryptr.encrypt(body.email),
+        image: cryptr.encrypt(body.image),
+        firstJobDay: body.firstJobDay,
+        totalWorkTime: cryptr.encrypt(body.totalWorkTime),
+        university: cryptr.encrypt(body.university),
+        graduationTime: body.graduationTime,
+        previousJob: cryptr.encrypt(body.previousJob),
+        skills: cryptr.encrypt(body.skills),
+        description: cryptr.encrypt(body.description),
+        createdAt: body.createdAt
+    }
+    return encryptedBody
+}
 
 module.exports = router

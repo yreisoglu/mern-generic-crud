41209a7fab0aca17c96ba938d39fdcd88cf994f1
Yasin Reisoğlu <100337130+yreisoglu@users.noreply.github.com>
2022-03-24 12:26:34 +0300

    bootstrap

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 require('dotenv').config()
 const bodyParser = require('body-parser');
-const database = require("./database/database")();
+//const database = require("./database/database")();
 
 const app = express();
 app.use(cors())
@@ -13,13 +13,6 @@ app.use(express.static(__dirname + "/client/build"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
 
 app.use((req, res, next) => {
     res.send('Welcome to Express');

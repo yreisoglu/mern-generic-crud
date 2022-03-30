edf531245e7b3064eda23806b0a17393ac720c44
Yasin Reisoğlu <100337130+yreisoglu@users.noreply.github.com>
2022-03-30 13:46:50 +0300

    staric file reference updated

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ app.use(cors())
 const port = process.env.PORT || 5000;
 
 
-app.use(express.static(__dirname + "/client/build"));
+//app.use(express.static(__dirname + "/client/build"));
+
+
+app.use(express.static("/client/build"));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+
 app.use('/img', express.static(__dirname + '/images'));
 
 

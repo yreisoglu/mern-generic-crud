6a76f20c5d3ab6c3817767aed496bad1583d0309
Yasin Reisoğlu <100337130+yreisoglu@users.noreply.github.com>
2022-03-30 14:01:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,15 +9,7 @@ app.use(cors())
 const port = process.env.PORT || 5000;
 
 
-//app.use(express.static(__dirname + "/client/build"));
-
-
-app.use(express.static("/client/build"));
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
-
-
+app.use(express.static(__dirname + "/client/build"));
 app.use('/img', express.static(__dirname + '/images'));
 
 

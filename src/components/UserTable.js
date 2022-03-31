935b8779fb1e5958ed4e95827d1c5320dd4a415d
Yasin Reisoğlu <100337130+yreisoglu@users.noreply.github.com>
2022-03-31 09:48:41 +0300

    doc update

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, forwardRef } from "react";
 import MaterialTable from "material-table";
 import { DeleteUsersByIds, GetUsers } from "../methods/GetUsers";
-
+import { isExpired } from '../methods/Account'
 
 import AddBox from '@material-ui/icons/AddBox';
 import ArrowDownward from '@material-ui/icons/ArrowDownward';
@@ -24,9 +24,12 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 
-import { Paragraph, Document, Packer } from "docx";
+import { Paragraph, Document, Packer, ImageRun, Table, TableRow, TableCell, RelativeVerticalPosition, WidthType, BorderStyle, TextRun, VerticalAlign } from "docx";
 import { saveAs } from "file-saver";
-import { Button } from "bootstrap";
+import { useNavigate } from "react-router-dom";
+
+
+import banner from '../Assets/banner.jpg'
 
 //import { render } from "express/lib/response";
 
@@ -57,11 +60,15 @@ const UserTable = () => {
 
   const [iserror, setIserror] = useState(false)
   const [errorMessages, setErrorMessages] = useState([])
+  const navigate = useNavigate();
 
   useEffect(() => {
-
+    isExpired().then(res => {
+      if (res) {
+        navigate("/users")
+      }
+    })
     GetUsers().then(response => {
-
       setData(response);
     });
   }, []);
@@ -74,9 +81,8 @@ const UserTable = () => {
       field: 'image',
       /* render: data => <img src={data.image} style={{width: 50, borderRadius: '50%'}}/>, */
       render: rowData => (
-        <img style={{ height: 36, borderRadius: '50%' }} src={"https://mern-generic-crud.herokuapp.com" + rowData.image}/>
+        <img style={{ height: 36, borderRadius: '50%' }} src={"https://mern-generic-crud.herokuapp.com" + rowData.image} />
       ),
-
 
     },
     {
@@ -126,15 +132,16 @@ const UserTable = () => {
 
   const [tableData, setTableData] = useState(columns);
 
-  const generate = (rowData) => {
+  const generate = async (rowData) => {
     let objArr = [];
     let obj = {};
+    console.log(rowData)
     for (let i = 0; i < rowData.length; i++) {
       obj['name'] = rowData[i].name;
       obj['surname'] = rowData[i].surname;
       obj['university'] = rowData[i].university;
       obj['description'] = rowData[i].description;
-
+      obj['image'] = "https://mern-generic-crud.herokuapp.com" + rowData[i].image
       let p = new Paragraph({
         text: obj['name'] + " " + obj['surname'],
         bullet: {
@@ -152,13 +159,131 @@ const UserTable = () => {
       objArr.push(s);
     }
 
-    const doc = new Document({
+    /* const doc = new Document({
       sections: [
         {
           children: objArr
         }
       ]
-    });
+    }); */
+    const awaitBanner = await fetch(banner);
+    console.log(obj['image'])
+    const image = await fetch(obj['image'])
+
+    const borders = {
+      top: {
+        style: BorderStyle.NONE,
+        size: 1,
+
+      },
+      bottom: {
+        style: BorderStyle.NONE,
+        size: 1,
+
+      },
+      left: {
+        style: BorderStyle.NONE,
+        size: 1,
+      },
+      right: {
+        style: BorderStyle.NONE,
+        size: 1,
+      },
+    };
+
+    const doc = new Document({
+      sections: [{
+        children: [
+          new Paragraph({
+            children: [
+              new ImageRun({
+                data: await awaitBanner.blob(),
+                transformation: {
+                  width: 795,
+                  height: 200
+
+                },
+
+                floating: {
+                  horizontalPosition: {
+                    offset: 1000, // relative: HorizontalPositionRelativeFrom.PAGE by default
+                  },
+                  verticalPosition: {
+                    offset: 1000, // relative: VerticalPositionRelativeFrom.PAGE by default
+                  },
+                }
+              })
+            ]
+          }),
+
+          new Paragraph({
+            children: [],  // Just newline without text
+          }), new Paragraph({
+            children: [],  // Just newline without text
+          }), new Paragraph({
+            children: [],  // Just newline without text
+          }), new Paragraph({
+            children: [],  // Just newline without text
+          }), new Paragraph({
+            children: [],  // Just newline without text
+          }), new Paragraph({
+            children: [],  // Just newline without text
+          }), new Paragraph({
+            children: [],  // Just newline without text
+          }), new Paragraph({
+            children: [],  // Just newline without text
+          }), new Paragraph({
+            children: [],  // Just newline without text
+          }),
+
+          new Table({
+            borders: borders,
+            rows: [
+              new TableRow({
+
+                children: [
+                  new TableCell({
+                    borders: borders,
+
+                    width: {
+                      size: 50,
+                      type: WidthType.PERCENTAGE
+                    },
+                    children: [
+                      new Paragraph({
+                        children: [
+                          new ImageRun({
+                            data: await image.blob(),
+                            transformation: {
+                              width: 165,
+                              height: 165
+                            },
+                          }),
+                        ]
+                      })
+                    ]
+                  }),
+                  new TableCell({
+                    borders: borders,
+                    verticalAlign: VerticalAlign.CENTER,
+                    children: [
+                      new Paragraph({
+                        children: [
+                          new TextRun({
+                            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum delectus, iure rem alias officia quaerat mollitia quibusdam excepturi! Sed, culpa. Accusantium ducimus commodi nisi quod nam aut excepturi ipsam culpa!"
+                          })
+                        ]
+                      })
+                    ]
+                  })
+                ]
+              })
+            ]
+          }),
+
+        ]
+      }]
+    })
 
     Packer.toBlob(doc).then((blob) => {
       console.log(blob);

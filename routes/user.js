e7d6484f499c6b3a089cb1287812e1f22b1b205e
Yasin Reisoğlu <100337130+yreisoglu@users.noreply.github.com>
2022-03-31 13:23:36 +0300

    replace method for private key

--- routes/user.js
+++ routes/user.js
@@ -14,7 +14,7 @@ const serviceAccount = {
     "type": process.env.type,
     "project_id": process.env.project_id,
     "private_key_id": process.env.private_key_id,
-    "private_key": process.env.private_key,
+    "private_key": process.env.private_key.replace(/\\n/g, '\n') ,
     "client_email": process.env.client_email,
     "client_id": process.env.client_id,
     "auth_uri": process.env.auth_uri,
@@ -22,7 +22,7 @@ const serviceAccount = {
     "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
     "client_x509_cert_url": process.env.client_x509_cert_url
 };
-
+console.log(serviceAccount.private_key.replace(/\\n/g, '\n') )
 const admin = firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert(serviceAccount)
 })

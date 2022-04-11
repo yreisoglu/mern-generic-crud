d14a6a0da32854b08957c038b79cd363fecea94b
Yasin Reisoğlu <100337130+yreisoglu@users.noreply.github.com>
2022-04-11 16:46:46 +0300

    update delete and get methods are finished.

--- routes/dynamic.js
+++ routes/dynamic.js
@@ -9,7 +9,7 @@ router.post("/create-form", (req, res) => {
         userDefinedFormPattern = req.body[0]
 
         if (!mongoose.models.formSchemas) {
-            createModel();
+            createSchemasModel();
         }
         const formSetupModel = mongoose.models.formSchemas
 
@@ -31,20 +31,44 @@ router.post("/create-form", (req, res) => {
 
 })
 
+router.get("/", (req, res) => {
+    try {
+        if (!mongoose.models.formSchemas) {
+            createSchemasModel();
+        }
+        mongoose.models.formSchemas.findOne({ formName: req.body.formName }).then(response => {
+            if (response) {
+                const formModel = getModel(response)
+                formModel.find().then(response => res.json(response)).catch(err => { console.log(err); res.status(400).json(err.errors.title.message) })
+
+                return
+            }
+            res.status(404).send("Form is unavailable");
+            return
+        }).catch(err => {
+            console.log(err);
+            res.status(404).send();
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(404).send();
+    }
+})
+
 router.post("/", (req, res) => {
     try {
         if (!mongoose.models.formSchemas) {
-            createModel();
+            createSchemasModel();
         }
         mongoose.models.formSchemas.findOne({ formName: req.body.formName }).then(response => {
-
-            const formSchema = new mongoose.Schema(response.formDetails);
-            if (!mongoose.models[response.formName]) {
-                mongoose.model(response.formName, formSchema);
+            if (response) {
+                const formModel = getModel(response)
+                const data = new formModel(req.body)
+                data.save().then((response) => res.json(response)).catch(err => { console.log(err); res.status(400).send(err.errors.title.message) })
+                return
             }
-            const formModel = mongoose.models[response.formName];
-            const modelTest = new formModel(req.body)
-            modelTest.save().then((response) => res.json(response)).catch(err => console.log(err))
+            res.status(404).send("Form is unavailable");
+            return
         }).catch(err => {
             console.log(err);
             res.status(404).send();
@@ -56,9 +80,63 @@ router.post("/", (req, res) => {
 
 })
 
+router.put("/", (req, res) => {
+    try {
+        if (!mongoose.models.formSchemas) {
+            createSchemasModel();
+        }
+        mongoose.models.formSchemas.findOne({ formName: req.body.formName }).then(response => {
+            if (response) {
+                const formModel = getModel(response)
+                formModel.findByIdAndUpdate(req.body._id, req.body).then(response => res.json(response)).catch(err => console.log(err))
+                return
+            }
+            res.status(404).send("Form is unavailable");
+            return
+        }).catch(err => {
+            console.log(err);
+            res.status(404).send();
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(404).send();
+    }
+})
 
 
-const createModel = () => {
+router.delete("/", (req, res) => {
+    try {
+        const ids = req.body["ids"];
+        if (!mongoose.models.formSchemas) {
+            createSchemasModel();
+        }
+        mongoose.models.formSchemas.findOne({ formName: req.body.formName }).then(response => {
+            if (response) {
+                const formModel = getModel(response)
+                formModel.deleteMany({ _id: { $in: ids } }).then(response => res.json(response)).catch(err => console.log(err))
+                return
+            }
+            res.status(404).send("Form is unavailable");
+            return
+        }).catch(err => {
+            console.log(err);
+            res.status(404).send();
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(404).send();
+    }
+})
+
+const getModel = (response) => {
+    const formSchema = new mongoose.Schema(response.formDetails);
+    if (!mongoose.models[response.formName]) {
+        mongoose.model(response.formName, formSchema);
+    }
+    return mongoose.models[response.formName];
+}
+
+const createSchemasModel = () => {
     var formUploadSetup = {
         formName: String,
         description: String,
